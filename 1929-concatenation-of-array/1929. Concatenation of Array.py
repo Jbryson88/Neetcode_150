364207aa1b519fed4f11874d371581110ddfa767
John Bryson <john.bryson710@gmail.com>
2024-05-17 20:53:58 -0500

    Runtime: 70 ms (56.36%), Memory: 17.1 MB (5.30%) - Gitcode

--- 1929-concatenation-of-array/1929. Concatenation of Array.py
+++ 1929-concatenation-of-array/1929. Concatenation of Array.py
@@ -1,10 +1,10 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        
+        ans = []
 
-        ans = nums*2
-
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
         return ans
-
 
         
